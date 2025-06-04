f853b0f52e995060411de47aa0572e8dccd48b2d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-04 17:38:16 -0400

    add command for creating a new character, private function for arranging ability scores

--- src-tauri/src/commands/characters.rs
+++ src-tauri/src/commands/characters.rs
@@ -1,4 +1,27 @@
+use crate::models::character;
+
 #[tauri::command]
-pub fn command_name() -> Result<(), String> {
+pub fn create() -> Result<(), String> {
+    let ability_scores = arrange_ability_scores();
     Ok(())
+}
+
+fn arrange_ability_scores() -> [character::AbilityScore; 6] {
+    let strength = character::AbilityScore::new(String::from("Strength"), String::from("STR"));
+    let dexterity = character::AbilityScore::new(String::from("Dexterity"), String::from("DEX"));
+    let constitution =
+        character::AbilityScore::new(String::from("Constitution"), String::from("CON"));
+    let intelligence =
+        character::AbilityScore::new(String::from("Intelligence"), String::from("INT"));
+    let wisdom = character::AbilityScore::new(String::from("Wisdom"), String::from("WIS"));
+    let charisma = character::AbilityScore::new(String::from("Charisma"), String::from("CHA"));
+
+    [
+        strength,
+        dexterity,
+        constitution,
+        intelligence,
+        wisdom,
+        charisma,
+    ]
 }
