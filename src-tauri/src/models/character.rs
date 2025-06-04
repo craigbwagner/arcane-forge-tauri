ec967c407f33537f3e6a5c762fced79478744905
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-04 17:50:59 -0400

    refactor character model to split combat stats into separate struct

--- src-tauri/src/models/character.rs
+++ src-tauri/src/models/character.rs
@@ -8,16 +8,7 @@ pub struct Character {
     pub classes: Vec<Class>,
     pub languages: Vec<String>,
     pub ability_scores: [AbilityScore; 6],
-    pub initiative: u8,
-    pub initiative_ods: u8,
-    pub speed: u8,
-    pub speed_mods: u8,
-    pub max_hp: u16,
-    pub current_hp: u16,
-    pub temp_hp: u8,
-    pub hit_dice_remaining: u8,
-    pub hit_dice_type: String,
-    pub hit_dice_total: u8,
+    pub combat_stats: CombatStats,
     pub additional_features: Vec<AdditionalFeature>,
     pub skills: [Skill; 18],
     pub class_features: Vec<ClassFeature>,
@@ -37,11 +28,24 @@ pub struct BasicDescription {
 }
 
 #[derive(Serialize, Deserialize)]
+pub struct CombatStats {
+    pub initiative_mods: u8,
+    pub speed: u8,
+    pub speed_mods: u8,
+    pub max_hp: u16,
+    pub current_hp: u16,
+    pub temp_hp: u8,
+    pub hit_dice_remaining: u8,
+    pub hit_dice_type: String,
+    pub hit_dice_total: u8,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct Class {
     pub name: String,
     pub subclass: String,
     pub level: u8,
-    pub class_feature: Vec<ClassFeature>,
+    pub class_features: Vec<ClassFeature>,
 }
 
 #[derive(Serialize, Deserialize)]
