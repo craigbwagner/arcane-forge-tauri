0f4cfd0e2b8ba407343fe6b7ed0404977c966026
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-04 19:32:42 -0400

    add dotenvy

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use dotenvy::dotenv;
+use std::env;
+
 fn main() {
     arcane_forge_lib::run()
 }
