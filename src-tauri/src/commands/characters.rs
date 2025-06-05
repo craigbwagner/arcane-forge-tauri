e9edca62e066dba95deaa44f559a0fd66017705b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-04 23:12:50 -0400

    remove arrange_abilities and arrange_skills, which are handled in the character model now

--- src-tauri/src/commands/characters.rs
+++ src-tauri/src/commands/characters.rs
@@ -1,46 +1,7 @@
 use crate::models::character;
 
 #[tauri::command]
-pub fn create() -> Result<(), String> {
-    let ability_scores = arrange_ability_scores();
-    let skills = arrange_skills();
-    Ok(())
-}
-
-fn arrange_ability_scores() -> [character::AbilityScore; 6] {
-    const NAMES: [(&str, &str); 6] = [
-        ("Strength", "STR"),
-        ("Dexterity", "DEX"),
-        ("Constitution", "CON"),
-        ("Intelligence", "INT"),
-        ("Wisdom", "WIS"),
-        ("Charisma", "CHA"),
-    ];
-
-    NAMES.map(|(name, short_name)| character::AbilityScore::new(name, short_name))
-}
-
-fn arrange_skills() -> [character::Skill; 18] {
-    const SKILLS: [(&str, &str); 18] = [
-        ("Acrobatics", "DEX"),
-        ("Animal Handling", "WIS"),
-        ("Arcana", "INT"),
-        ("Athletics", "STR"),
-        ("Deception", "CHA"),
-        ("History", "INT"),
-        ("Insight", "WIS"),
-        ("Intimidation", "CHA"),
-        ("Investigation", "INT"),
-        ("Medicine", "WIS"),
-        ("Nature", "INT"),
-        ("Perception", "WIS"),
-        ("Performance", "CHA"),
-        ("Persuasion", "CHA"),
-        ("Religion", "INT"),
-        ("Sleight of Hand", "DEX"),
-        ("Stealth", "DEX"),
-        ("Survival", "WIS"),
-    ];
-
-    SKILLS.map(|(name, ability)| character::Skill::new(name, ability))
+pub fn create() -> Result<character::Character, String> {
+    let new_character = character::Character::default();
+    Ok(new_character)
 }
