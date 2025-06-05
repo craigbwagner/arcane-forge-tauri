683b306334441302c44853829b87670342f940c7
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-04 22:15:02 -0400

    add arrange_skills function for initializing all skills on character creation

--- src-tauri/src/commands/characters.rs
+++ src-tauri/src/commands/characters.rs
@@ -3,6 +3,7 @@ use crate::models::character;
 #[tauri::command]
 pub fn create() -> Result<(), String> {
     let ability_scores = arrange_ability_scores();
+    let skills = arrange_skills();
     Ok(())
 }
 
@@ -16,5 +17,30 @@ fn arrange_ability_scores() -> [character::AbilityScore; 6] {
         ("Charisma", "CHA"),
     ];
 
-    NAMES.map(|(name, abbr)| character::AbilityScore::new(name.into(), abbr.into()))
+    NAMES.map(|(name, short_name)| character::AbilityScore::new(name, short_name))
+}
+
+fn arrange_skills() -> [character::Skill; 18] {
+    const SKILLS: [(&str, &str); 18] = [
+        ("Acrobatics", "DEX"),
+        ("Animal Handling", "WIS"),
+        ("Arcana", "INT"),
+        ("Athletics", "STR"),
+        ("Deception", "CHA"),
+        ("History", "INT"),
+        ("Insight", "WIS"),
+        ("Intimidation", "CHA"),
+        ("Investigation", "INT"),
+        ("Medicine", "WIS"),
+        ("Nature", "INT"),
+        ("Perception", "WIS"),
+        ("Performance", "CHA"),
+        ("Persuasion", "CHA"),
+        ("Religion", "INT"),
+        ("Sleight of Hand", "DEX"),
+        ("Stealth", "DEX"),
+        ("Survival", "WIS"),
+    ];
+
+    SKILLS.map(|(name, ability)| character::Skill::new(name, ability))
 }
