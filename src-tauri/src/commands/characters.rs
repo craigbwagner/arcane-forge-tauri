593dc1547362d9a92cd2431f0c88b5ddc27590d6
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-05 00:00:04 -0400

    add test for create_character method to ensure that it returns an Ok response

--- src-tauri/src/commands/characters.rs
+++ src-tauri/src/commands/characters.rs
@@ -5,3 +5,14 @@ pub fn create_character() -> Result<character::Character, String> {
     let new_character = character::Character::default();
     Ok(new_character)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_result_is_ok() {
+        let result = create_character();
+        assert!(result.is_ok());
+    }
+}
