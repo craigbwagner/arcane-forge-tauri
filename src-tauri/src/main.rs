48dca61052dc1399ac3c7693a7a2f9068cb198c8
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-04 22:15:42 -0400

    remove dotenv from scope

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use dotenvy::dotenv;
-use std::env;
-
 fn main() {
     arcane_forge_lib::run()
 }
