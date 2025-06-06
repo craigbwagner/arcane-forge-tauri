d3d29fd50867cf9f3096457eca9b3ed1aaa32423
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-06 17:43:47 -0400

    update main function to return a result with color_eyre as error type

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
+use color_eyre::eyre;
+
+fn main() -> Result<(), eyre::Report> {
     arcane_forge_lib::run()
 }
