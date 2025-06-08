43d72a672960dc9147a27279af2851c26d2f1a26
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-07 22:08:42 -0400

    remove PartialEq, Clone, default implementation

--- src-tauri/src/models/class.rs
+++ src-tauri/src/models/class.rs
@@ -1,24 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Class {
     pub id: Option<i64>,
     pub name: String,
     pub subclass: String,
     pub level: u8,
-    pub class_features: Vec<i64>,
-    pub subclass_features: Vec<i64>,
-}
-
-impl Default for Class {
-    fn default() -> Self {
-        Self {
-            id: None,
-            name: String::new(),
-            subclass: String::new(),
-            level: 1,
-            class_features: Vec::new(),
-            subclass_features: Vec::new(),
-        }
-    }
+    pub class_feature_ids: Vec<i64>,
+    pub subclass_ids: Vec<i64>,
 }
