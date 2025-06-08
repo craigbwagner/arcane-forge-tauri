e0afec09b1c50759d2b1768dcfd50fcaf931b219
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-07 20:02:08 -0400

    add serde for serialization of errors

--- src-tauri/src/errors.rs
+++ src-tauri/src/errors.rs
@@ -1,6 +1,8 @@
+use serde::Serialize;
 use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, Serialize)]
+#[serde(tag = "type", content = "message")]
 pub enum AppError {
     #[error("Character creation failed: {0}")]
     CharacterCreationError(String),
