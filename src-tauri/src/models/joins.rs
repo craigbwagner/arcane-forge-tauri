cb4d07d24eb93741a1664e93dba02460e9784b1d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-07 21:59:16 -0400

    add CharacterFeature join

--- src-tauri/src/models/joins.rs
+++ src-tauri/src/models/joins.rs
@@ -1,11 +1,14 @@
 pub struct CharacterItem {
     pub character_id: i64,
-    pub item_id: i64
+    pub item_id: i64,
 }
 
 pub struct CharacterSpell {
     pub character_id: i64,
-    pub item_id: i64
+    pub item_id: i64,
 }
 
-pub struct
+pub struct CharacterFeature {
+    pub character_id: i64,
+    pub item_id: i64,
+}
