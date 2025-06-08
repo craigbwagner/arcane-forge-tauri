7c757906fd556e7f35f38a7aecbe059b0b301c42
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-07 20:05:55 -0400

    add characters page and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { CharactersComponent } from "./pages/characters/characters.component";
 
 export const routes: Routes = [
   {
@@ -11,4 +12,8 @@ export const routes: Routes = [
     path: 'dashboard',
     component: DashboardComponent
   },
+  {
+    path: 'characters',
+    component: CharactersComponent
+  }
 ];
