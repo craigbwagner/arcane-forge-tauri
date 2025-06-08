960df9f9d2048ddbbe7345e2e02802d43e58b86d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-07 23:48:16 -0400

    use Feature and Subclass in Class struct

--- src-tauri/src/models/class.rs
+++ src-tauri/src/models/class.rs
@@ -1,11 +1,13 @@
 use serde::{Deserialize, Serialize};
 
+use crate::models::feature::Feature;
+use crate::models::subclass::Subclass;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Class {
     pub id: Option<i64>,
     pub name: String,
-    pub subclass: String,
     pub level: u8,
-    pub class_feature_ids: Vec<i64>,
-    pub subclass_ids: Vec<i64>,
+    pub class_features: Vec<Feature>,
+    pub subclasses: Vec<Subclass>,
 }
