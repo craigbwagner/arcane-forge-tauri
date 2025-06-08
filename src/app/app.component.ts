d23800e543d27b53ab44b4e4a6f5643eb74b35da
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-07 20:01:02 -0400

    create pages and components folders, move Angular components to their corresponding folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { invoke } from "@tauri-apps/api/core";
-import { NavbarComponent } from "./navbar/navbar.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: AppComponent
-  }
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
 ];
