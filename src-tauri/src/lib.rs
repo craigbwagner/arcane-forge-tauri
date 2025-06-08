5ee046d96f015f4f4796cdb51fc339ff50c660bd
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-08 18:31:00 -0400

    add db connection and creation of characters table if it doesn't exist at tauri startup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,34 @@
+use std::sync::{Arc, Mutex};
+
+use rusqlite::Connection;
+
 mod commands;
-mod db;
 mod dtos;
 mod errors;
 mod models;
+mod repositories;
 mod services;
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() -> Result<(), color_eyre::Report> {
     color_eyre::install()?;
 
+    let db = Connection::open("character.db")?;
+
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS characters (
+            id TEXT PRIMARY KEY,
+            name TEXT NOT NULL,
+            alignment TEXT NOT NULL,
+            created_at TEXT NOT NULL
+        )",
+        [],
+    )?;
+
+    let db = Arc::new(Mutex::new(db));
+
     tauri::Builder::default()
+        .manage(db)
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
