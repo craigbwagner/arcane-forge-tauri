0f3decfde05c397e2963211cd7d6671a2e8028ec
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-11 19:56:03 -0400

    refacter create to return newly generated character immediately without saving to database

--- src-tauri/src/services/character_service.rs
+++ src-tauri/src/services/character_service.rs
@@ -1,10 +1,9 @@
+use crate::dtos::character_dtos::FullCharacterData;
 use crate::errors::AppError;
-use crate::repositories::character_repository;
 use crate::services::mappings::character_mapper;
 use rusqlite::Connection;
 
-pub fn create(conn: &Connection) -> Result<i64, AppError> {
+pub fn create(conn: &Connection) -> Result<FullCharacterData, AppError> {
     let new_character = character_mapper::new()?;
-    let id = character_repository::insert_character(conn, new_character)?;
-    Ok(id)
+    Ok(new_character)
 }
