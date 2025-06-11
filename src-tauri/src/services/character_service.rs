6b38b80f873df76320445013e44aac08af6f7413
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-11 19:30:11 -0400

    refactor to use free functions instead of static methods

--- src-tauri/src/services/character_service.rs
+++ src-tauri/src/services/character_service.rs
@@ -1,10 +1,10 @@
 use crate::errors::AppError;
 use crate::repositories::character_repository;
-use crate::services::mappings::character_mapper::CharacterMapper;
+use crate::services::mappings::character_mapper;
 use rusqlite::Connection;
 
 pub fn create(conn: &Connection) -> Result<i64, AppError> {
-    let new_character = CharacterMapper::new()?;
+    let new_character = character_mapper::new()?;
     let id = character_repository::insert_character(conn, new_character)?;
     Ok(id)
 }
