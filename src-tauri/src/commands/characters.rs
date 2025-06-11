c7f5805eb8254d4bb5f5b6b9033fe3b944e81c17
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-11 19:59:46 -0400

    refactor create_character to return FullCharacterData type instead of a character db ID

--- src-tauri/src/commands/characters.rs
+++ src-tauri/src/commands/characters.rs
@@ -3,12 +3,15 @@ use std::sync::{Arc, Mutex};
 use rusqlite::Connection;
 use tauri::State;
 
+use crate::dtos::character_dtos::FullCharacterData;
 use crate::errors::AppError;
 use crate::services::character_service;
 
 #[tauri::command]
-pub fn create_character(db: State<'_, Arc<Mutex<Connection>>>) -> Result<i64, AppError> {
+pub fn create_character(
+    db: State<'_, Arc<Mutex<Connection>>>,
+) -> Result<FullCharacterData, AppError> {
     let conn = db.lock().unwrap();
-    let new_character_id = character_service::create(&conn)?;
-    Ok(new_character_id)
+    let new_character = character_service::create(&conn)?;
+    Ok(new_character)
 }
