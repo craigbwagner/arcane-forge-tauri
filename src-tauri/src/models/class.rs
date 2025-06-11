7c4b3ba799fc5501660cc05b3dfe54661f811d4b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-11 19:03:37 -0400

    refactor to use String instead of struct vectors to function as a database model

--- src-tauri/src/models/class.rs
+++ src-tauri/src/models/class.rs
@@ -1,13 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-use crate::models::feature::Feature;
-use crate::models::subclass::Subclass;
-
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Class {
     pub id: Option<i64>,
     pub name: String,
-    pub level: u8,
-    pub class_features: Vec<Feature>,
-    pub subclasses: Vec<Subclass>,
+    pub class_features: String,
+    pub subclasses: String,
+    pub hit_die: String,
+    pub primary_ability: String, // dex, str, char, int, wis, or
+    pub saving_throw_proficiencies: String, // deserialized into string
 }
