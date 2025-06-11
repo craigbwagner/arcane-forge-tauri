35bba57a550dfe7da66467b69adaf1b93d60f71b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-10 23:25:30 -0400

    add sql query for inserting new character into database

--- src-tauri/src/repositories/character_repository.rs
+++ src-tauri/src/repositories/character_repository.rs
@@ -1,7 +1,52 @@
-use rusqlite::Connection;
+use rusqlite::{params, Connection, Result};
+use tauri::App;
 
 use crate::{errors::AppError, models::character::Character};
 
-pub fn insert_character(conn: &Connection, dto: Character) -> Result<i64, AppError> {
-    Ok(0)
+pub fn insert_character(conn: &Connection, character: Character) -> Result<i64, AppError> {
+    dbg!(&character);
+    let rows_affected = conn.execute(
+        "INSERT INTO characters (
+            name,
+            levels,
+            creator,
+            basic_description,
+            classes,
+            languages,
+            ability_scores,
+            combat_stats,
+            additional_features,
+            skills,
+            items,
+            kill_list,
+            created_at,
+            updated_at
+        ) VALUES (
+            ?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14
+        )",
+        params![
+            character.name,
+            character.levels,
+            character.creator,
+            character.basic_description,
+            character.classes,
+            character.languages,
+            character.ability_scores,
+            character.combat_stats,
+            character.additional_features,
+            character.skills,
+            character.items,
+            character.kill_list,
+            character.created_at,
+            character.updated_at,
+        ],
+    );
+
+    match rows_affected {
+        Err(e) => Err(AppError::CharacterCreationError(format!(
+            "Failed to insert character in db: {}",
+            e
+        ))),
+        Ok(_) => Ok(conn.last_insert_rowid()),
+    }
 }
