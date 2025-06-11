08170ecdeaa059405f066268165df06eda851e34
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-11 19:12:29 -0400

    remove insertion of now non-existent fields in the character model from sql queries

--- src-tauri/src/repositories/character_repository.rs
+++ src-tauri/src/repositories/character_repository.rs
@@ -7,34 +7,26 @@ pub fn insert_character(conn: &Connection, character: Character) -> Result<i64, 
     let rows_affected = conn.execute(
         "INSERT INTO characters (
             name,
-            levels,
             creator,
             basic_description,
-            classes,
             languages,
             ability_scores,
             combat_stats,
-            additional_features,
             skills,
-            items,
             kill_list,
             created_at,
             updated_at
         ) VALUES (
-            ?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14
+            ?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10
         )",
         params![
             character.name,
-            character.levels,
             character.creator,
             character.basic_description,
-            character.classes,
             character.languages,
             character.ability_scores,
             character.combat_stats,
-            character.additional_features,
             character.skills,
-            character.items,
             character.kill_list,
             character.created_at,
             character.updated_at,
