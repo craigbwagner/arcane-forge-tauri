501ba5c78ee3ac26219dc4c4cb8831da7b104d7a
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-11 19:07:30 -0400

    move all serialized data structures to character_dtos from character model module, add structs for data transfer to character_dtos module

--- src-tauri/src/models/character.rs
+++ src-tauri/src/models/character.rs
@@ -1,136 +1,16 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum Ability {
-    Strength,
-    Dexterity,
-    Constitution,
-    Intelligence,
-    Wisdom,
-    Charisma,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum Alignment {
-    LawfulGood,
-    NeutralGood,
-    ChaoticGood,
-    LawfulNeutral,
-    TrueNeutral,
-    ChaoticNeutral,
-    LawfulEvil,
-    NeutralEvil,
-    ChaoticEvil,
-}
-
-impl std::fmt::Display for Alignment {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Alignment::LawfulGood => "Lawful Good",
-                Alignment::NeutralGood => "Neutral Good",
-                Alignment::ChaoticGood => "Chaotic Good",
-                Alignment::LawfulNeutral => "Lawful Neutral",
-                Alignment::TrueNeutral => "Neutral",
-                Alignment::ChaoticNeutral => "Chaotic Neutral",
-                Alignment::LawfulEvil => "Lawful Evil",
-                Alignment::NeutralEvil => "Neutral Evil",
-                Alignment::ChaoticEvil => "Chaotic Evil",
-            }
-        )
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-pub enum Size {
-    Tiny,
-    Small,
-    Medium,
-    Large,
-    Huge,
-    Gargantuan,
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-pub enum Sex {
-    Male,
-    Female,
-    Other,
-    Unspecified,
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-pub enum HitDiceType {
-    D6,
-    D8,
-    D10,
-    D12,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
 pub struct Character {
     pub id: Option<i64>,
     pub name: String,
-    pub levels: String,
     pub creator: String,
-    pub basic_description: String,
-    pub classes: String,
+    pub basic_description: String, // BasicDescription deserialized
+    pub combat_stats: String,
     pub languages: String,
     pub ability_scores: String,
-    pub combat_stats: String,
-    pub additional_features: String,
     pub skills: String,
-    pub items: String,
     pub kill_list: String,
     pub created_at: String,
     pub updated_at: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct BasicDescription {
-    pub race: String,
-    pub sex: Sex,
-    pub size: Size,
-    pub age: u16,
-    pub height: String,
-    pub weight: u16,
-    pub alignment: Alignment,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct CombatStats {
-    pub initiative_mods: u8,
-    pub speed: u8,
-    pub speed_mods: u8,
-    pub max_hp: u16,
-    pub current_hp: u16,
-    pub temp_hp: u8,
-    pub hit_dice_remaining: u8,
-    pub hit_dice_type: HitDiceType,
-    pub hit_dice_total: u8,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct AbilityScore {
-    pub name: String,
-    pub short_name: String,
-    pub is_proficient: bool,
-    pub score: u8,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Skill {
-    pub name: String,
-    pub is_proficient: bool,
-    pub has_expertise: bool,
-    pub ability_name: Ability,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct LevelEntry {
-    pub class: Option<String>,
-    pub level: u8,
-    pub subclass: Option<String>,
 }
--- src-tauri/src/models/feature.rs
+++ src-tauri/src/models/feature.rs
@@ -1,18 +1,16 @@
 use serde::{Deserialize, Serialize};
 
-use crate::models::{ActionType, UseReset};
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Feature {
-    pub id: i64,                 // SQLite INTEGER PRIMARY KEY AUTOINCREMENT
-    pub character_ids: Vec<i64>, // FK to Character.id's
+    pub id: i64,
     pub name: Option<String>,
     pub description: Option<String>,
-    pub uses: Option<u8>,
-    pub uses_reset_on: Option<UseReset>, // store enum as string
-    pub action_type: Option<ActionType>, // store enum as string
+    pub max_uses: Option<u8>,
+    pub uses_reset_on: Option<String>, // store enum as string
+    pub action_type: Option<String>,   // store enum as string
     pub duration: Option<String>,
+    pub feature_type: Option<String>,
     pub source: Option<String>,
-    pub level_acquired: Option<u8>,
+    pub level_requirement: Option<u8>,
     pub is_passive: bool,
 }
