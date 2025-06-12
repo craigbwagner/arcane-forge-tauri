a0df1a83a3b1e2300f959545a1da066a3f446756
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-11 23:55:07 -0400

    refactor db.lock() error handling for a more robust solution

--- src-tauri/src/commands/characters.rs
+++ src-tauri/src/commands/characters.rs
@@ -17,13 +17,12 @@ pub fn create_character() -> Result<FullCharacterData, AppError> {
 async fn save_new_character(
     character: FullCharacterData,
     db: State<'_, Arc<Mutex<Connection>>>,
-) -> Result<(), String> {
-    let conn = match db.lock() {
-        Ok(conn) => conn,
-        Err(e) => Err(AppError::DatabaseConnectionError(e.to_string())),
-    };
+) -> Result<(), AppError> {
+    let conn = db
+        .lock()
+        .map_err(|e| AppError::DatabaseConnectionError(e.to_string()))?;
 
-    let new_character_id = character_service::save_new_character(character, conn);
+    let new_character_id = character_service::save_new_character(character, &conn)?;
 
     Ok(())
 }
