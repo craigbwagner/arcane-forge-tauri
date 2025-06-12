0d85904b3e4b5d9e305597f7db8c4179112c033c
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-12 10:38:18 -0400

    fix instances where Navbar is still being referenced as NavbarComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { invoke } from "@tauri-apps/api/core";
-import { Navbar } from "./components/navbar";
+import { Navbar } from "./components/navbar/navbar.component";
 
 @Component({
   selector: "app-root",
