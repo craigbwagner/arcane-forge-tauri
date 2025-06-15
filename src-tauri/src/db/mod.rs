1eb0830cb493d789cfd729b9e905e2712a15f86a
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-15 15:22:28 -0400

    move db initialization to its own module

---
+++ src-tauri/src/db/mod.rs
@@ -0,0 +1,38 @@
+use std::sync::{Arc, Mutex};
+
+use rusqlite::Connection;
+
+use crate::errors::AppError;
+
+pub fn initialize() -> Result<Arc<Mutex<Connection>>, AppError> {
+    let conn = match Connection::open("arcane-forge.db") {
+        Ok(conn) => conn,
+        Err(e) => return Err(AppError::DatabaseConnectionError(e.to_string())),
+    };
+
+    match conn.execute(
+        "CREATE TABLE IF NOT EXISTS characters (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            levels TEXT,
+            creator TEXT,
+            basic_description TEXT,
+            classes TEXT,
+            languages TEXT,
+            ability_scores TEXT,
+            combat_stats TEXT,
+            additional_features TEXT,
+            skills TEXT,
+            items TEXT,
+            kill_list TEXT,
+            created_at TEXT,
+            updated_at TEXT
+        )",
+        [],
+    ) {
+        Err(e) => return Err(AppError::DatabaseConnectionError(e.to_string())),
+        _ => (),
+    };
+
+    Ok(Arc::new(Mutex::new(conn)))
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ use rusqlite::Connection;
 use crate::commands::character_commands;
 
 mod commands;
+mod db;
 mod dtos;
 mod errors;
 mod models;
@@ -15,30 +16,7 @@ mod services;
 pub fn run() -> Result<(), color_eyre::Report> {
     color_eyre::install()?;
 
-    let db = Connection::open("arcane-forge.db")?;
-
-    db.execute(
-        "CREATE TABLE IF NOT EXISTS characters (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            levels TEXT,
-            creator TEXT,
-            basic_description TEXT,
-            classes TEXT,
-            languages TEXT,
-            ability_scores TEXT,
-            combat_stats TEXT,
-            additional_features TEXT,
-            skills TEXT,
-            items TEXT,
-            kill_list TEXT,
-            created_at TEXT,
-            updated_at TEXT
-        )",
-        [],
-    )?;
-
-    let db = Arc::new(Mutex::new(db));
+    let database = db::initialize()?;
 
     tauri::Builder::default()
         .manage(db)
