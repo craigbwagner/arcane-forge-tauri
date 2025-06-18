1a9d1d63f59ec1a6816ac8134f53f0d5f4fe09ee
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-17 20:17:46 -0400

    refactor character_service to use injected repository instance

--- src-tauri/src/services/character_service.rs
+++ src-tauri/src/services/character_service.rs
@@ -1,7 +1,9 @@
 use crate::dtos::character_dtos::FullCharacterData;
 use crate::errors::AppError;
+use crate::repositories::character_repository::CharacterRepository;
 use crate::services::mappings::character_mapper;
-use rusqlite::Connection;
+
+pub fn get_all(repo: &CharacterRepository) -> Result<Vec<FullCharacterData>, AppError> {}
 
 pub fn create() -> Result<FullCharacterData, AppError> {
     let new_character = character_mapper::new()?;
@@ -9,6 +11,6 @@ pub fn create() -> Result<FullCharacterData, AppError> {
 }
 
 pub fn save_new(data: FullCharacterData, repo: &CharacterRepository) -> Result<i64, AppError> {
-    let character_db_model = character_mapper::dto_to_db(data);
-    Ok(9)
+    let character_db_model = character_mapper::dto_to_db(data)?;
+    repo.insert_character(character_db_model)
 }
