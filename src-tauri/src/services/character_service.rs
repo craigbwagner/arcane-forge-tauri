c7f6e375779c3da6860cddcad007529fed5485db
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-17 20:50:56 -0400

    refactor save_new to use insert method on repo instead of insert_character

--- src-tauri/src/services/character_service.rs
+++ src-tauri/src/services/character_service.rs
@@ -2,8 +2,11 @@ use crate::dtos::character_dtos::FullCharacterData;
 use crate::errors::AppError;
 use crate::repositories::character_repository::CharacterRepository;
 use crate::services::mappings::character_mapper;
+use crate::traits::repository::Repository;
 
-pub fn get_all(repo: &CharacterRepository) -> Result<Vec<FullCharacterData>, AppError> {}
+pub fn get_all(repo: &CharacterRepository) -> Result<Vec<FullCharacterData>, AppError> {
+    let characters = repo.get_all();
+}
 
 pub fn create() -> Result<FullCharacterData, AppError> {
     character_mapper::new()
@@ -11,5 +14,5 @@ pub fn create() -> Result<FullCharacterData, AppError> {
 
 pub fn save_new(data: FullCharacterData, repo: &CharacterRepository) -> Result<i64, AppError> {
     let character_db_model = character_mapper::dto_to_db(data)?;
-    repo.insert_character(character_db_model)
+    repo.insert(character_db_model)
 }
