92acfd02aeadefe42d99fbf489562101dfbfce04
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-25 17:41:50 -0400

    add item_repo to app state

--- src-tauri/src/app_state.rs
+++ src-tauri/src/app_state.rs
@@ -1,5 +1,8 @@
-use crate::repositories::character_repository::CharacterRepository;
+use crate::repositories::{
+    character_repository::CharacterRepository, item_repository::ItemRepository,
+};
 
 pub struct AppState {
     pub character_repo: CharacterRepository,
+    pub item_repo: ItemRepository,
 }
