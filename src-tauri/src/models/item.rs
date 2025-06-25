d16aa4d8521daa6ae074e5131511d8f6abc4250b
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-25 18:07:06 -0400

    refactor all Item fields to remove Option wrappers around types

--- src-tauri/src/models/item.rs
+++ src-tauri/src/models/item.rs
@@ -2,15 +2,15 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Item {
-    pub name: Option<String>,
-    pub description: Option<String>,
-    pub total_charges: Option<u8>,
-    pub value: Option<u16>,
-    pub weight: Option<f32>,
-    pub rarity: Option<String>,
-    pub item_type: Option<String>,
-    pub attunement: Option<bool>,
+    pub name: String,
+    pub description: String,
+    pub total_charges: u8,
+    pub value: u16,
+    pub weight: f32,
+    pub rarity: String,
+    pub item_type: String,
+    pub attunement: bool,
     pub is_magical: bool,
-    pub properties: Option<String>,
-    pub acquired_through: Option<String>,
+    pub properties: String,
+    pub acquired_through: String,
 }
