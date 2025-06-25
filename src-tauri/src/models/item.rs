3ba7c6298186983f6e9e4f8135b63d5c9ae0c514
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-06-25 18:09:21 -0400

    add id field

--- src-tauri/src/models/item.rs
+++ src-tauri/src/models/item.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Item {
+    pub id: Option<i64>,
     pub name: String,
     pub description: String,
     pub total_charges: u8,
