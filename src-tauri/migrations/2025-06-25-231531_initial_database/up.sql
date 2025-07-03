d6aaa2f2499ce288cf5420d878c1f79ab385e042
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-02 20:58:46 -0400

    add character table creation to first migration

--- src-tauri/migrations/2025-06-25-231531_initial_database/up.sql
+++ src-tauri/migrations/2025-06-25-231531_initial_database/up.sql
@@ -1,1 +1,17 @@
--- Your SQL goes here
+CREATE TABLE IF NOT EXISTS characters (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            levels TEXT,
+            creator TEXT,
+            basic_description TEXT,
+            classes TEXT,
+            languages TEXT,
+            ability_scores TEXT,
+            combat_stats TEXT,
+            additional_features TEXT,
+            skills TEXT,
+            items TEXT,
+            kill_list TEXT,
+            created_at TEXT,
+            updated_at TEXT
+        )
