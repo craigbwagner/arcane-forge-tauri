35ce947b1e76731a1183bfa2160e6848c7be0a9c
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-04 16:54:00 -0400

    refactor Repository trait to take database connection reference as parameters for methods

--- src-tauri/src/traits/repository.rs
+++ src-tauri/src/traits/repository.rs
@@ -1,9 +1,17 @@
+use std::sync::{Arc, Mutex};
+
+use diesel::SqliteConnection;
+
 use crate::errors::AppError;
 
 pub trait Repository<T> {
-    fn get_all(&self) -> Result<Vec<T>, AppError>;
-    fn get_by_id(&self, id: i32) -> Result<Option<T>, AppError>;
-    fn insert(&self, entity: T) -> Result<i32, AppError>;
-    fn update(&self, entity: T) -> Result<(), AppError>;
-    fn delete(&self, id: i32) -> Result<(), AppError>;
+    fn get_all(&self, conn: &Arc<Mutex<SqliteConnection>>) -> Result<Vec<T>, AppError>;
+    fn get_by_id(
+        &self,
+        conn: &Arc<Mutex<SqliteConnection>>,
+        id: i32,
+    ) -> Result<Option<T>, AppError>;
+    fn insert(&self, conn: &Arc<Mutex<SqliteConnection>>, entity: T) -> Result<i32, AppError>;
+    fn update(&self, conn: &Arc<Mutex<SqliteConnection>>, entity: T) -> Result<(), AppError>;
+    fn delete(&self, conn: &Arc<Mutex<SqliteConnection>>, id: i32) -> Result<(), AppError>;
 }
