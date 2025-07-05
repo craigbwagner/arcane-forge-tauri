962c81424cb8595eb615af6b6ff388fecebd7be1
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-05 17:48:14 -0400

    fix usage of Character type instead of generic type T in insert return type

--- src-tauri/src/traits/repository.rs
+++ src-tauri/src/traits/repository.rs
@@ -7,7 +7,7 @@ use crate::errors::AppError;
 pub trait Repository<T, U> {
     fn get_all(conn: &Arc<Mutex<SqliteConnection>>) -> Result<Vec<T>, AppError>;
     fn get_by_id(conn: &Arc<Mutex<SqliteConnection>>, id: i32) -> Result<Option<T>, AppError>;
-    fn insert(conn: &Arc<Mutex<SqliteConnection>>, entity: U) -> Result<Character, AppError>;
+    fn insert(conn: &Arc<Mutex<SqliteConnection>>, entity: U) -> Result<T, AppError>;
     fn update(conn: &Arc<Mutex<SqliteConnection>>, entity: T) -> Result<(), AppError>;
     fn delete(conn: &Arc<Mutex<SqliteConnection>>, id: i32) -> Result<(), AppError>;
     fn get_connection(
