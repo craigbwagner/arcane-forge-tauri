11bb14defde3bf1b960551aa88fd8778edee6d94
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-05 19:37:22 -0400

    generate intial schema

--- src-tauri/src/schema.rs
+++ src-tauri/src/schema.rs
@@ -16,3 +16,24 @@ diesel::table! {
         updated_at -> Text,
     }
 }
+
+diesel::table! {
+    items (id) {
+        id -> Integer,
+        name -> Text,
+        description -> Text,
+        total_charges -> Integer,
+        value -> Integer,
+        weight -> Float,
+        rarity -> Text,
+        item_type -> Text,
+        attunement -> Bool,
+        is_magical -> Bool,
+        acquired_through -> Text,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    characters,
+    items,
+);
