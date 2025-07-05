23960ac95bacbaa31f04a3ad71fa80e22f65801d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-04 22:24:23 -0400

    remove columns that will be related data

--- src-tauri/migrations/2025-06-25-231531_initial_database/up.sql
+++ src-tauri/migrations/2025-06-25-231531_initial_database/up.sql
@@ -1,16 +1,13 @@
 CREATE TABLE IF NOT EXISTS characters (
             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            levels TEXT NOT NULL,
             creator TEXT NOT NULL,
             basic_description TEXT NOT NULL,
-            classes TEXT NOT NULL,
+            levels TEXT NOT NULL,
+            combat_stats TEXT NOT NULL,
             languages TEXT NOT NULL,
             ability_scores TEXT NOT NULL,
-            combat_stats TEXT NOT NULL,
-            additional_features TEXT NOT NULL,
             skills TEXT NOT NULL,
-            items TEXT NOT NULL,
             kill_list TEXT NOT NULL,
             created_at TEXT NOT NULL,
             updated_at TEXT NOT NULL
