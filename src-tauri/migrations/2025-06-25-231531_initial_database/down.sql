8e7b90bfa170e206a313ece9905555e304681edb
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-05 19:40:16 -0400

    add items table to migration

--- src-tauri/migrations/2025-06-25-231531_initial_database/down.sql
+++ src-tauri/migrations/2025-06-25-231531_initial_database/down.sql
@@ -1,1 +1,2 @@
-DROP TABLE characters
+DROP TABLE characters;
+DROP TABLE items;
--- src-tauri/migrations/2025-06-25-231531_initial_database/up.sql
+++ src-tauri/migrations/2025-06-25-231531_initial_database/up.sql
@@ -11,4 +11,18 @@ CREATE TABLE IF NOT EXISTS characters (
             kill_list TEXT NOT NULL,
             created_at TEXT NOT NULL,
             updated_at TEXT NOT NULL
-        )
+        );
+
+CREATE TABLE IF NOT EXISTS items (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            total_charges INTEGER NOT NULL,
+            value INTEGER NOT NULL,
+            weight REAL NOT NULL,
+            rarity TEXT NOT NULL,
+            item_type TEXT NOT NULL,
+            attunement BOOLEAN NOT NULL,
+            is_magical BOOLEAN NOT NULL,
+            acquired_through TEXT NOT NULL
+        );
