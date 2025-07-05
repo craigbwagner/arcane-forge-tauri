570b980e964cc7ad1a82f4ec5e6a1e05fceb016d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-04 22:09:39 -0400

    add NewCharacter struct for inserting a new character without an existing ID

--- src-tauri/src/models/character.rs
+++ src-tauri/src/models/character.rs
@@ -17,3 +17,19 @@ pub struct Character {
     pub created_at: String,
     pub updated_at: String,
 }
+
+#[derive(Insertable, AsChangeset, Deserialize, Debug)]
+#[diesel(table_name = crate::schema::characters)]
+pub struct NewCharacter {
+    pub name: Option<String>,
+    pub levels: Option<String>,
+    pub creator: Option<String>,
+    pub basic_description: Option<String>,
+    pub combat_stats: Option<String>,
+    pub languages: Option<String>,
+    pub ability_scores: Option<String>,
+    pub skills: Option<String>,
+    pub kill_list: Option<String>,
+    pub created_at: Option<String>,
+    pub updated_at: Option<String>,
+}
