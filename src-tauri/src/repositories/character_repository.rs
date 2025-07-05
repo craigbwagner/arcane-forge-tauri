ec583f0b818f8e9edf1fb1895feaa1367d640632
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-04 23:05:34 -0400

    add RunQueryDsl import and refactor insert method to return Character on success

--- src-tauri/src/repositories/character_repository.rs
+++ src-tauri/src/repositories/character_repository.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, Mutex};
 
-use diesel::{SelectableHelper, SqliteConnection};
+use diesel::{RunQueryDsl, SelectableHelper, SqliteConnection};
 
 use crate::{
     errors::AppError,
@@ -26,13 +26,18 @@ impl Repository<Character, NewCharacter> for CharacterRepository {
     fn insert(
         conn: &Arc<Mutex<SqliteConnection>>,
         character: NewCharacter,
-    ) -> Result<i32, AppError> {
+    ) -> Result<Character, AppError> {
         let mut conn = Self::get_connection(conn)?;
 
         diesel::insert_into(characters::table)
             .values(character)
             .returning(Character::as_returning())
             .get_result(&mut *conn)
+            .map_err(|e| {
+                AppError::DatabaseOperationError(
+                    "Could not insert new character into database.".to_string(),
+                )
+            })
     }
 
     fn update(conn: &Arc<Mutex<SqliteConnection>>, entity: Character) -> Result<(), AppError> {
