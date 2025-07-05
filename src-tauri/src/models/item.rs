75d57010b15b563678921b5c900c309d463b9819
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-05 19:38:38 -0400

    convert types on Item and NewItem to be compatible with diesel

--- src-tauri/src/models/item.rs
+++ src-tauri/src/models/item.rs
@@ -1,12 +1,30 @@
+use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Queryable, Selectable)]
+#[diesel(table_name = crate::schema::items)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Item {
-    pub id: Option<i64>,
     pub name: String,
     pub description: String,
-    pub total_charges: u8,
-    pub value: u16,
+    pub total_charges: i32,
+    pub value: i32,
+    pub weight: f32,
+    pub rarity: String,
+    pub item_type: String,
+    pub attunement: bool,
+    pub is_magical: bool,
+    pub acquired_through: String,
+}
+
+#[derive(Insertable, AsChangeset, Deserialize, Debug)]
+#[diesel(table_name = crate::schema::items)]
+pub struct NewItem {
+    pub id: i32,
+    pub name: String,
+    pub description: String,
+    pub total_charges: i32,
+    pub value: i32,
     pub weight: f32,
     pub rarity: String,
     pub item_type: String,
