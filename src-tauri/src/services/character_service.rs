cc6138318889b919f811538f2cef190d80b51472
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-05 20:32:46 -0400

    pass reference to new_character as argument to db_to_dto instead of passing ownership

--- src-tauri/src/services/character_service.rs
+++ src-tauri/src/services/character_service.rs
@@ -14,7 +14,7 @@ use crate::traits::repository::Repository;
 pub fn create(state: State<'_, AppState>) -> Result<FullCharacterData, AppError> {
     let new_character_data = character_mapper::new()?;
     let new_character = CharacterRepository::insert(&state.db, new_character_data)?;
-    character_mapper::db_to_dto(new_character)
+    character_mapper::db_to_dto(&new_character)
 }
 
 pub fn update(
