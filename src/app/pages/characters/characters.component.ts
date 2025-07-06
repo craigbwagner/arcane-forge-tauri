cd5467676975b55a02909db3ba2fa3fbfe52de8a
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-05 21:26:37 -0400

    add fetchCharacters function and refactor createCharacter to return a Promise containing FullCharacterData instead of void

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-import { invoke } from '@tauri-apps/api/core';
+import { Component } from "@angular/core";
+import { invoke } from "@tauri-apps/api/core";
+import { FullCharacterData } from "../../types/character/FullCharacterData";
 
 @Component({
-  selector: 'app-characters',
+  selector: "app-characters",
   standalone: true,
   imports: [],
-  templateUrl: './characters.component.html',
-  styleUrl: './characters.component.css'
+  templateUrl: "./characters.component.html",
+  styleUrl: "./characters.component.css",
 })
 export class CharactersComponent {
-  async createCharacter() : Promise<void> {
-    let newCharacterId: number = await invoke('create_character');
+  async fetchCharacters(): Promise<FullCharacterData> {
+    return await invoke("get_all_characters");
+  }
+
+  async createCharacter(): Promise<FullCharacterData> {
+    return await invoke("create_character");
   }
 }
