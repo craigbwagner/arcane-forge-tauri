f2bf2c4a4cd730c0d3e751a074a381d06e4b94c4
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-05 20:43:51 -0400

    remove default greeting from AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,4 @@ import { Navbar } from "./components/navbar/navbar.component";
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
-export class AppComponent {
-  greetingMessage = "";
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
-}
+export class AppComponent {}
