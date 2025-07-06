a47ee86516a8a4f31982fb6ea491528da5cc15b3
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-05 20:35:03 -0400

    uncomment get_all_characters command

--- src-tauri/src/commands/character_commands.rs
+++ src-tauri/src/commands/character_commands.rs
@@ -5,12 +5,12 @@ use crate::dtos::character_dtos::FullCharacterData;
 use crate::errors::AppError;
 use crate::services::character_service;
 
-// #[tauri::command]
-// pub async fn get_all_characters(
-//     app_state: State<'_, AppState>,
-// ) -> Result<Vec<FullCharacterData>, AppError> {
-//     character_service::get_all(&app_state.character_repo)
-// }
+#[tauri::command]
+pub async fn get_all_characters(
+    state: State<'_, AppState>,
+) -> Result<Vec<FullCharacterData>, AppError> {
+    character_service::get_all(state)
+}
 
 #[tauri::command]
 pub async fn create_character(state: State<'_, AppState>) -> Result<FullCharacterData, AppError> {
