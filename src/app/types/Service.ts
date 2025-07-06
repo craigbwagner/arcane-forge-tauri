4be4cc98616e88ce5237a8584c37eb726ff8bbfb
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-05 21:44:30 -0400

    add create function

--- src/app/types/Service.ts
+++ src/app/types/Service.ts
@@ -2,4 +2,5 @@ import { FullCharacterData } from "./character/FullCharacterData";
 
 export interface Service {
   getAll(): Promise<FullCharacterData[]>;
+  create(): Promise<FullCharacterData>;
 }
