691592c6903231ffd9dc41c0cd82919bc072a5d0
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-06 21:48:53 -0400

    import RouterModule for links to character sheets

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 import { FullCharacterData } from "../../types/character/FullCharacterData";
 import { CharacterService } from "../../services/character.service";
 
 @Component({
   selector: "app-characters",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: "./characters.component.html",
   styleUrl: "./characters.component.css",
 })
