d15cfb7c9350685bfb47c41e02bc7000e6477f04
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-12 22:45:40 -0400

    set default values for new properties in AbilityScore, proficiency_bonus, Skill, and initiative in CombatStats

--- src-tauri/src/services/mappings/character_mapper.rs
+++ src-tauri/src/services/mappings/character_mapper.rs
@@ -76,6 +76,7 @@ pub fn db_to_dto(data: &Character) -> Result<FullCharacterData, AppError> {
         id: data.id,
         name: data.name.clone(),
         creator: data.creator.clone(),
+        proficiency_bonus: 2,
         basic_description,
         combat_stats,
         languages,
@@ -95,36 +96,48 @@ fn initial_ability_scores() -> [AbilityScore; 6] {
         short_name: "STR".into(),
         is_proficient: false,
         score: 10,
+        base_modifier: 0,
+        additional_mods: 0,
     };
     let dexterity = AbilityScore {
         name: "Dexterity".into(),
         short_name: "DEX".into(),
         is_proficient: false,
         score: 10,
+        base_modifier: 0,
+        additional_mods: 0,
     };
     let constitution = AbilityScore {
         name: "Constitution".into(),
         short_name: "CON".into(),
         is_proficient: false,
         score: 10,
+        base_modifier: 0,
+        additional_mods: 0,
     };
     let intelligence = AbilityScore {
         name: "Intelligence".into(),
         short_name: "INT".into(),
         is_proficient: false,
         score: 10,
+        base_modifier: 0,
+        additional_mods: 0,
     };
     let wisdom = AbilityScore {
         name: "Wisdom".into(),
         short_name: "WIS".into(),
         is_proficient: false,
         score: 10,
+        base_modifier: 0,
+        additional_mods: 0,
     };
     let charisma = AbilityScore {
         name: "Charism".into(),
         short_name: "CHA".into(),
         is_proficient: false,
         score: 10,
+        base_modifier: 0,
+        additional_mods: 0,
     };
 
     [
@@ -143,108 +156,126 @@ fn initial_skills() -> [Skill; 18] {
         ability_name: Ability::Dexterity,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let animal_handling = Skill {
         name: "Animal Handling".into(),
         ability_name: Ability::Wisdom,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let arcana = Skill {
         name: "Arcana".into(),
         ability_name: Ability::Intelligence,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let athletics = Skill {
         name: "Athletics".into(),
         ability_name: Ability::Strength,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let deception = Skill {
         name: "Deception".into(),
         ability_name: Ability::Charisma,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let history = Skill {
         name: "History".into(),
         ability_name: Ability::Intelligence,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let insight = Skill {
         name: "Insight".into(),
         ability_name: Ability::Wisdom,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let intimidation = Skill {
         name: "Intimidation".into(),
         ability_name: Ability::Charisma,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let investigation = Skill {
         name: "Investigation".into(),
         ability_name: Ability::Intelligence,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let medicine = Skill {
         name: "Medicine".into(),
         ability_name: Ability::Wisdom,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let nature = Skill {
         name: "Nature".into(),
         ability_name: Ability::Intelligence,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let perception = Skill {
         name: "Perception".into(),
         ability_name: Ability::Wisdom,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let performance = Skill {
         name: "Performance".into(),
         ability_name: Ability::Charisma,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let persuasion = Skill {
         name: "Persuasion".into(),
         ability_name: Ability::Charisma,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let religion = Skill {
         name: "Religion".into(),
         ability_name: Ability::Intelligence,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let sleight_of_hand = Skill {
         name: "Sleight of Hand".into(),
         ability_name: Ability::Dexterity,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let stealth = Skill {
         name: "Stealth".into(),
         ability_name: Ability::Dexterity,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
     let survival = Skill {
         name: "Survival".into(),
         ability_name: Ability::Wisdom,
         is_proficient: false,
         has_expertise: false,
+        additional_mods: 0,
     };
 
     [
@@ -283,6 +314,7 @@ fn initial_basic_description() -> BasicDescription {
 
 fn initial_combat_stats() -> CombatStats {
     CombatStats {
+        initiative: 0,
         initiative_mods: 0,
         speed: 30,
         speed_mods: 0,
