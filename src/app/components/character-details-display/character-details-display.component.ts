666471d23f2ddb9053e8350a04d72da015097427
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-12 23:12:52 -0400

    move code mistakenly placed in character-details-display for fetching character to character-sheet component

--- src/app/components/character-details-display/character-details-display.component.ts
+++ src/app/components/character-details-display/character-details-display.component.ts
@@ -1,47 +1,14 @@
-import { Component, effect, inject, OnInit, signal } from "@angular/core";
-import { FullCharacterData } from "../../types/character/FullCharacterData";
+import { FullCharacterData } from "./../../types/character/FullCharacterData";
+import { Component, Input, WritableSignal } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ActivatedRoute, Router, RouterModule } from "@angular/router";
-import { CharacterService } from "../../services/character.service";
-import { toSignal } from "@angular/core/rxjs-interop";
 
 @Component({
 	selector: "app-character-details-display",
 	standalone: true,
-	imports: [CommonModule, RouterModule],
+	imports: [CommonModule],
 	templateUrl: "./character-details-display.component.html",
 	styleUrl: "./character-details-display.component.css",
 })
-export class CharacterDetailsDisplayComponent implements OnInit {
-	private router = inject(Router);
-	private activatedRoute = inject(ActivatedRoute);
-	private characterService = inject(CharacterService);
-	private routeParams = toSignal(this.activatedRoute.paramMap);
-
-	character = signal<FullCharacterData | undefined>(undefined);
-	loading = signal(false);
-	error = signal<string | null>(null);
-
-	ngOnInit() {
-		const params = this.routeParams();
-		const id = params?.get("id");
-		if (id) {
-			this.loadCharacter(+id);
-		}
-	}
-
-	async loadCharacter(id: number) {
-		this.loading.set(true);
-		this.error.set(null);
-
-		try {
-			const character = await this.characterService.getById(id);
-			this.character.set(character);
-		} catch (e) {
-			this.error.set("Failed to load character.");
-			console.error(e);
-		} finally {
-			this.loading.set(false);
-		}
-	}
+export class CharacterDetailsDisplayComponent{
+  @Input() character!: WritableSignal<FullCharacterData | undefined>;
 }
--- src/app/pages/character-sheet/character-sheet.component.ts
+++ src/app/pages/character-sheet/character-sheet.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CharacterDetailsDisplayComponent } from '../../components/character-details-display/character-details-display.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Router, ActivatedRoute, RouterModule } from '@angular/router';
+import { CharacterService } from '../../services/character.service';
+import { FullCharacterData } from '../../types/character/FullCharacterData';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-character-sheet',
   standalone: true,
-  imports: [CharacterDetailsDisplayComponent],
+  imports: [CharacterDetailsDisplayComponent, RouterModule, CommonModule],
   templateUrl: './character-sheet.component.html',
   styleUrl: './character-sheet.component.css'
 })
 export class CharacterSheetComponent {
+  private router = inject(Router);
+	private activatedRoute = inject(ActivatedRoute);
+	private characterService = inject(CharacterService);
+	private routeParams = toSignal(this.activatedRoute.paramMap);
 
+	character = signal<FullCharacterData | undefined>(undefined);
+	loading = signal(false);
+	error = signal<string | null>(null);
+
+	ngOnInit() {
+		const params = this.routeParams();
+		const id = params?.get("id");
+		if (id) {
+			this.loadCharacter(+id);
+		}
+	}
+
+	async loadCharacter(id: number) {
+		this.loading.set(true);
+		this.error.set(null);
+
+		try {
+			const character = await this.characterService.getById(id);
+			this.character.set(character);
+		} catch (e) {
+			this.error.set("Failed to load character.");
+			console.error(e);
+		} finally {
+			this.loading.set(false);
+		}
+	}
 }
