393ce754bc05f4a7928e44635e4841d48caed944
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-17 17:20:58 -0400

    refactor character details display component to be wrapped in a primeng card component

--- src/app/components/character-details-display/character-details-display.component.ts
+++ src/app/components/character-details-display/character-details-display.component.ts
@@ -1,13 +1,14 @@
 import { FullCharacterData } from "./../../types/character/FullCharacterData";
-import { Component, Input, WritableSignal } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { Card } from "primeng/card";
 
 
 @Component({
-    selector: "app-character-details-display",
-    imports: [],
-    templateUrl: "./character-details-display.component.html",
-    styleUrl: "./character-details-display.component.css"
+	selector: "app-character-details-display",
+	imports: [Card],
+	templateUrl: "./character-details-display.component.html",
+	styleUrl: "./character-details-display.component.css",
 })
-export class CharacterDetailsDisplayComponent{
-  @Input() character!: FullCharacterData;
+export class CharacterDetailsDisplayComponent {
+	@Input() character!: FullCharacterData;
 }
