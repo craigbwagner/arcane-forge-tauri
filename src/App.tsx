c50d31a92a3c967d8a55ae5d6faf3552b98843eb
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-18 15:25:16 -0400

    add route to CharactersList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import PageLayout from './layouts/PageLayout'
+import CharactersList from './pages/CharactersList'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<PageLayout />}>
         <Route index element={<Dashboard />} />
+        <Route path="characters" element={<CharactersList />} />
       </Route>
     </Routes>
   )
