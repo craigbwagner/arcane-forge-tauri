8707a7952066fe4dd1f66dc36cb1da8021e144f8
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-20 00:01:11 -0400

    add CharacterSheet component and route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import PageLayout from './layouts/PageLayout'
 import CharactersList from './pages/CharactersList'
+import CharacterSheet from './pages/CharacterSheet'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<PageLayout />}>
         <Route index element={<Dashboard />} />
         <Route path="characters-list" element={<CharactersList />} />
+        <Route path="character/:id" element={<CharacterSheet />} />
       </Route>
     </Routes>
   )
