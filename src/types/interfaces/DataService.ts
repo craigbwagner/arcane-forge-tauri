8894c90b34f5a67504c20fdb0cc84fe5946369fe
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-29 21:33:11 -0400

    add create method to DataService interface

--- src/types/interfaces/DataService.ts
+++ src/types/interfaces/DataService.ts
@@ -1,4 +1,5 @@
 export default interface DataService<T> {
-  getAll(): Promise<T[]>;
-  getById(id: number): Promise<T>;
+	getAll(): Promise<T[]>;
+	getById(id: number): Promise<T>;
+	create(): Promise<T>;
 }
