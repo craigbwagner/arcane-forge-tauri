520fc906c69952197a0073913ee1cd619b7ae4e7
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-07-29 22:46:58 -0400

    remove unused imports

--- src/components/character-sheet/CharacterBasicDescription.tsx
+++ src/components/character-sheet/CharacterBasicDescription.tsx
@@ -1,7 +1,4 @@
-import { useEffect } from "react";
-import useCharacterStore from "../../stores/characterStore";
 import { BasicDescription } from "../../types/character/BasicDescription";
-import { useParams } from "react-router-dom";
 import { Card } from "@chakra-ui/react";
 
 export default function CharacterBasicDescription({
@@ -14,18 +11,18 @@ export default function CharacterBasicDescription({
 	return (
 		<section>
 			<Card.Root>
-				<Card.Title className="text-center">
+				<Card.Title className='text-center'>
 					<h1>{name}</h1>
 				</Card.Title>
-        <Card.Description className="grid grid-cols-3">
-          <p>{basicDescription.race}</p>
-          <p>{basicDescription.sex}</p>
-          <p>{basicDescription.alignment}</p>
-          <p>{basicDescription.size}</p>
-          <p>{basicDescription.age}</p>
-          <p>{basicDescription.height}</p>
-          <p>{basicDescription.weight}</p>
-        </Card.Description>
+				<Card.Description className='grid grid-cols-3'>
+					<p>{basicDescription.race}</p>
+					<p>{basicDescription.sex}</p>
+					<p>{basicDescription.alignment}</p>
+					<p>{basicDescription.size}</p>
+					<p>{basicDescription.age}</p>
+					<p>{basicDescription.height}</p>
+					<p>{basicDescription.weight}</p>
+				</Card.Description>
 			</Card.Root>
 		</section>
 	);
